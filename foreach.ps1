c04c52d661cdc0d62c672d6c6d2b7d0c1e7ff3a7
Alvin Hedman <alvin.hedman.ga.ntig.elev.se>
2023-09-14 15:08:31 +0200

    loopar

---
+++ foreach.ps1
@@ -0,0 +1,4 @@
+$names = @("Martin", "Jesper", "Alvin");
+foreach($name in $names){
+    write-output $name;
+}
---
+++ while.ps1
@@ -0,0 +1,5 @@
+$counter = 0;
+while ($counter -lt 20){
+    $counter = $counter +1;
+    Write-Output $counter;
+}
