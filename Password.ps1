cbef6060390717a6cc1afe6987548bbf7f007cfe
Alvin Hedman <alvin.hedman.ga.ntig.elev.se>
2023-10-05 15:08:57 +0200

    Password

--- Password.ps1
+++ Password.ps1
@@ -1,9 +1,9 @@
-$characters = "a","b","c","d","e","f","g","h"
+$characters = "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9"
 
-$Password =
+$Password = ""
 
 for ($i = 0;$i -lt 8;$i++){
-    $randomnum = Get-Random -max 8
+    $randomnum = Get-Random -max 35
     $password += $characters[$randomnum]
 }
 
