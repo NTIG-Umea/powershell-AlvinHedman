ffe18c6407945c23fb8e2e150ac5598405a47ff2
Alvin Hedman <alvin.hedman@elev.ga.ntig.se>
2024-04-19 12:59:30 +0200

    hjk

--- ADUser.ps1
+++ ADUser.ps1
@@ -1,1 +1,8 @@
-﻿New-ADUser -Name "Anders Andersson" -AccountPassword (ConvertTo-SecureString "Andand06" -AsPlainText -Force) -Enabled @true -path "OU=Alvins users, DC=Walters, DC=Labb"
+﻿#New-ADUser -Name "Anders Andersson" -AccountPassword (ConvertTo-SecureString "Andand06" -AsPlainText -Force) -Enabled @true -path "OU=Alvins users, DC=Walters, DC=Labb"
+
+$users = Import-Csv .\Usernames.csv
+
+foreach($user in $users){
+    $username = $user.firstname + "." + $user.lastname
+    New-ADUser -Name $username -path "OU=Alvins users, DC=Walters, DC=Labb"
+}
